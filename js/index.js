4097072a7598e2b4b9ae08ad952e32c30f7786bd
Heroel Carpinetti <heroeljcarpinetti@gmail.com>
2020-10-30 19:45:37 -0300

    Scroll de planes a contacto

--- js/index.js
+++ js/index.js
@@ -1,1 +1,14 @@
 "use strict";
+
+document.addEventListener('DOMContentLoaded', () => {
+    
+    let btnContratar = document.getElementsByClassName("btn-contratar");
+    
+    for(let i = 0; i < btnContratar.length; i++){
+        btnContratar[i].addEventListener('click', () => {
+            document.getElementById('contacto').scrollIntoView({block: "center", behavior: "smooth"})
+        })
+    }
+
+    
+})
