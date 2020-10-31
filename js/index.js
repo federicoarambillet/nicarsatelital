034cbd7287c1e99e5b104dcb9f9100fb0caabc50
Heroel Carpinetti <heroeljcarpinetti@gmail.com>
2020-10-31 18:48:25 -0300

    Scroll nav

--- js/index.js
+++ js/index.js
@@ -4,11 +4,22 @@ document.addEventListener('DOMContentLoaded', () => {
     
     let btnContratar = document.getElementsByClassName("btn-contratar");
     
+    /* Asigno la funcion de scroll a las cards de planes */
     for(let i = 0; i < btnContratar.length; i++){
         btnContratar[i].addEventListener('click', () => {
             document.getElementById('contacto').scrollIntoView({block: "center", behavior: "smooth"})
         })
     }
 
+    let btnNavegacion = document.getElementsByClassName('nav-link');
+    
+    /* Asigno a los nav los efectos de scroll */
+    for(let i = 0; i < btnNavegacion.length; i++){
+        btnNavegacion[i].addEventListener('click', navegacion);
+    }
     
 })
+
+function navegacion(){
+    document.getElementById(this.getAttribute('value')).scrollIntoView({block: "center", behavior: "smooth"});
+}
